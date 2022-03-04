9db9818514547500925a2278c789a2b0695c270e
Anurag Kumar <anuragkr41@gmail.com>
2022-03-05 02:31:03 +0530

    Deploy code Fix

--- src/main/java/com/avanse/springboot/controller/globalPages/MiscellaneousFormsController.java
+++ src/main/java/com/avanse/springboot/controller/globalPages/MiscellaneousFormsController.java
@@ -2,15 +2,29 @@ package com.avanse.springboot.controller.globalPages;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.avanse.springboot.DTO.forms.miscellaneous.AssociateWithUsDTO;
 import com.avanse.springboot.DTO.forms.miscellaneous.CSRLeadsDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.ENachDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.MoratoriumDeregisterDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.MoratoriumFacilityDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.RestructuringOfLoansDTO;
 import com.avanse.springboot.model.forms.miscellaneous.AssociateWithUs;
 import com.avanse.springboot.model.forms.miscellaneous.CSRLeads;
+import com.avanse.springboot.model.forms.miscellaneous.ENach;
+import com.avanse.springboot.model.forms.miscellaneous.MoratoriumDeregister;
+import com.avanse.springboot.model.forms.miscellaneous.MoratoriumFacility;
+import com.avanse.springboot.model.forms.miscellaneous.RestructuringOfLoans;
 import com.avanse.springboot.service.forms.miscellaneous.AssociateWithUsService;
 import com.avanse.springboot.service.forms.miscellaneous.CSRLeadsService;
+import com.avanse.springboot.service.forms.miscellaneous.ENachService;
+import com.avanse.springboot.service.forms.miscellaneous.MoratoriumDeregisterService;
+import com.avanse.springboot.service.forms.miscellaneous.MoratoriumFacilityService;
+import com.avanse.springboot.service.forms.miscellaneous.RestructuringOfLoansService;
 
 @Controller
 public class MiscellaneousFormsController {
@@ -20,7 +34,18 @@ public class MiscellaneousFormsController {
 	
 	@Autowired
 	CSRLeadsService csrLeadsService;
+
+	@Autowired
+	ENachService eNachService;
 	
+	@Autowired
+	RestructuringOfLoansService restructuringOfLoansService;
+	
+	@Autowired
+	MoratoriumFacilityService moratoriumFacilityService ;
+	
+	@Autowired
+	MoratoriumDeregisterService moratoriumDeregisterService;
 	
 	@PostMapping("/associate-with-us/add")
 	public String associateWithUsPost(@ModelAttribute("associateWithUsDTO") AssociateWithUsDTO associateWithUsDTO ) {
@@ -34,6 +59,7 @@ public class MiscellaneousFormsController {
 		return "dynamicPages/thankyou";
 		
 	}
+	
 	@PostMapping("/csr/add")
 	public String csrPost(@ModelAttribute("csrLeadsDTO") CSRLeadsDTO csrLeadsDTO) {
 		CSRLeads csrLeads = new CSRLeads();
@@ -43,11 +69,91 @@ public class MiscellaneousFormsController {
 		csrLeads.setName(csrLeadsDTO.getName());
 		csrLeads.setSubject(csrLeadsDTO.getSubject());
 		csrLeadsService.addCSRLead(csrLeads);
-				
 		return "dynamicPages/thankyou";
-		
 	}
 	
-
+	@PostMapping("/e-nach/add")
+	public String eNachPost(@ModelAttribute("eNachDTO") ENachDTO eNachDTO) {
+		ENach eNach = new ENach();
+		eNach.setDateOfBirthOrIncorporation(eNachDTO.getDateOfBirthOrIncorporation());
+		eNach.setEmail(eNachDTO.getEmail());
+		eNach.setLoanAccountNumber(eNachDTO.getLoanAccountNumber().toUpperCase());
+		eNach.setMobileNumber(eNachDTO.getMobileNumber());
+		eNach.setPanNumber(eNachDTO.getPanNumber().toUpperCase());
+		eNachService.addEnach(eNach);
+		return "dynamicPages/thankyou";
+	}
 	
+	@PostMapping("/restructuring-of-loans-step1/add")
+	public String restructuringOfLoansPost(@ModelAttribute("restructuringOfLoansDTO") RestructuringOfLoansDTO restructuringOfLoansDTO ) {
+		RestructuringOfLoans restructuringOfLoans = new RestructuringOfLoans();
+		restructuringOfLoans.setDateOfBirthOrIncorporation(restructuringOfLoansDTO.getDateOfBirthOrIncorporation());
+		restructuringOfLoans.setEmail(restructuringOfLoansDTO.getEmail());
+		restructuringOfLoans.setLoanAccountNumber(restructuringOfLoansDTO.getLoanAccountNumber().toUpperCase());
+		restructuringOfLoans.setMobileNumber(restructuringOfLoansDTO.getMobileNumber());
+		restructuringOfLoans.setPanNumber(restructuringOfLoansDTO.getPanNumber().toUpperCase());
+		restructuringOfLoansService.addRestructuringOfLoans(restructuringOfLoans);
+		return "dynamicPages/thankyou";
+	}
+	
+	@PostMapping("/moratorium-facility-2020/add")
+	public String MoratoriumFacilityPost(@ModelAttribute("moratoriumFacilityDTO") MoratoriumFacilityDTO moratoriumFacilityDTO ) {
+		MoratoriumFacility moratoriumFacility = new MoratoriumFacility();
+		moratoriumFacility.setAlternateMobileNumber(moratoriumFacilityDTO.getAlternateMobileNumber());
+		moratoriumFacility.setDateOfBirthOrIncorporation(moratoriumFacilityDTO.getDateOfBirthOrIncorporation());
+		moratoriumFacility.setLoanAccountNumber(moratoriumFacilityDTO.getLoanAccountNumber());
+		moratoriumFacility.setPanNumber(moratoriumFacilityDTO.getPanNumber());
+		moratoriumFacilityService.addMoratoriumFacility(moratoriumFacility);
+		return "dynamicPages/thankyou";
+	}
+	
+	@PostMapping("/moratorium-facility-deregister/add")
+	public String moratoriumFacilityDeregister(@ModelAttribute("moratoriumDeregisterDTO") MoratoriumDeregisterDTO moratoriumDeregisterDTO ) {
+		MoratoriumDeregister moratoriumDeregister = new MoratoriumDeregister();
+		moratoriumDeregister.setAlternateNumber(moratoriumDeregisterDTO.getAlternateNumber());
+		moratoriumDeregister.setDateOfBirthOrIncorporation(moratoriumDeregisterDTO.getDateOfBirthOrIncorporation());
+		moratoriumDeregister.setEmail(moratoriumDeregisterDTO.getEmail());
+		moratoriumDeregister.setLoanAccountNumber(moratoriumDeregisterDTO.getLoanAccountNumber());
+		moratoriumDeregister.setMobileNumber(moratoriumDeregisterDTO.getMobileNumber());
+		moratoriumDeregister.setName(moratoriumDeregisterDTO.getName());
+		moratoriumDeregisterService.addMoratariumDeregister(moratoriumDeregister);
+		return "dynamicPages/thankyou";
+	}
+	
+	@GetMapping("/admin/miscellaneous-forms/associate-with-us")
+	public String associateWithUsAdminGet(Model model) {
+		model.addAttribute("associateWithUsLeads", associateWithUsService.getAssociateWithUsLeads());
+		return "associate-with-us-leads";
+	} 
+	
+	@GetMapping("/admin/miscellaneous-forms/csr")
+	public String csrAdminGet(Model model) {
+		model.addAttribute("csrLeads", csrLeadsService.getCSRLeads());
+		return "csr-leads";
+	} 
+//	here is copy
+	
+	@GetMapping("/admin/miscellaneous-forms/e-nach")
+	public String eNachAdminGet(Model model) {
+		model.addAttribute("eNachDetails", eNachService.getEnachDetails());
+		return "eNach-details";
+	} 
+	
+	@GetMapping("/admin/miscellaneous-forms/restructuring-of-loans-step1")
+	public String restructuringOfLoansAdminGet(Model model) {
+		model.addAttribute("restructuringOfLoansDetails", restructuringOfLoansService.getRestructuringOfLoanDetails());
+		return "restructuring-of-loans-details";
+	} 
+	
+	@GetMapping("/admin/miscellaneous-forms/moratorium-facility-2020")
+	public String moratoriaumFacilityAdminGet(Model model) {
+		model.addAttribute("moratoriumFacilityDetails", moratoriumFacilityService.getMoratoriumFacilityDetails());
+		return "moratorium-facility-2020-details";
+	} 
+	
+	@GetMapping("/admin/miscellaneous-forms/moratorium-facility-deregister")
+	public String moratoriumFacilityDeregisterAdminGet(Model model) {
+		model.addAttribute("moratoriumFacilityDeregisterDetails", moratoriumDeregisterService.getMoratoriumDeregisterDetails());
+		return "moratorium-facility-deregister-details";
+	} 
 }
--- src/main/java/com/avanse/springboot/controller/globalPages/PageController.java
+++ src/main/java/com/avanse/springboot/controller/globalPages/PageController.java
@@ -27,6 +27,10 @@ import com.avanse.springboot.DTO.forms.contactUs.InvestorDTO;
 import com.avanse.springboot.DTO.forms.contactUs.MediaDTO;
 import com.avanse.springboot.DTO.forms.miscellaneous.AssociateWithUsDTO;
 import com.avanse.springboot.DTO.forms.miscellaneous.CSRLeadsDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.ENachDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.MoratoriumDeregisterDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.MoratoriumFacilityDTO;
+import com.avanse.springboot.DTO.forms.miscellaneous.RestructuringOfLoansDTO;
 import com.avanse.springboot.model.Page;
 import com.avanse.springboot.model.Post;
 import com.avanse.springboot.model.PostCategory;
@@ -74,6 +78,21 @@ public class PageController {
 	public String studyAbroadPage(Model model) {
 		return "dynamicPages/study-abroad";
 	}
+	@GetMapping(value={"/education-loan/study-india-loan"})
+	public String studyInIndiaPage(Model model) {
+		model.addAttribute("testimonials", testimonialService.getAllTestimonials());
+
+		return "dynamicPages/study-in-india";
+	}
+	
+	@GetMapping(value={"/education-loan-for-mba"})
+	public String educationLoanForMbaPage(Model model) {
+		model.addAttribute("testimonials", testimonialService.getAllTestimonials());
+		
+		return "dynamicPages/education-loan-for-mba";
+	}
+	
+	
 	@GetMapping("/career")
 	public String careerPage() {
 		return "dynamicPages/career";
@@ -88,6 +107,14 @@ public class PageController {
 		return "about";
 	}
 	
+	
+	@GetMapping("/testimonials")
+	public String testimonialsPage(Model model) {
+		model.addAttribute("testimonials", testimonialService.getAllTestimonials());
+
+		return "dynamicPages/testimonials";
+	}
+		
 	@GetMapping("/courseDetail/{courseId}")
 	public String courseDetailsPage(@PathVariable long courseId, Model model) {
 		model.addAttribute("course", courseService.getCourseById(courseId).get());
@@ -140,7 +167,6 @@ public class PageController {
 		return modelAndView;
 	}
 	
-	
 	@GetMapping("/contact-us")
 	public ModelAndView getContactUsCustomer(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/contactus");
@@ -151,37 +177,39 @@ public class PageController {
 		return modelAndView;
 	}
 	
-	
 	  @GetMapping("/apply-now") 
 	  public ModelAndView getApplyNowForm(Model model) {
 	  ModelAndView modelAndView = new ModelAndView("dynamicPages/apply-now");
 	  model.addAttribute("applyNowGeneralDTO", new ApplyNowGeneralDTO()); 
-	  return modelAndView;
-	  
+		model.addAttribute("awards", awardService.getAllAwards() );
+
+	  return modelAndView;  
 	  }
 	 
 	@GetMapping("/apply-for-education-institution-loan")
 	public ModelAndView getEducationInstitutionLoan(Model model) {
 		ModelAndView modelAndView =  new ModelAndView("dynamicPages/education-institution-loan");
 		model.addAttribute("educationInstitutionLoanDTO", new EducationInstitutionLoanDTO());
-		return modelAndView;
-		
+		model.addAttribute("awards", awardService.getAllAwards() );
+
+		return modelAndView;	
 	}
-	
 	
 	@GetMapping("/apply-for-executive-education-loan")
 	public ModelAndView getExecutiveEducationLoan(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/executive-education-loan");
 		model.addAttribute("executiveEducationLoanDTO", new ExecutiveEducationLoanDTO());
+
+		model.addAttribute("awards", awardService.getAllAwards() );
+
 		return modelAndView;
 	}
-	
-	
 	
 	@GetMapping("/apply-for-e-tutoring")
 	public ModelAndView getApplyForEtutoringPage(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/apply-for-e-tutoring");
 		model.addAttribute("eTutoringDTO", new ETutoringDTO());
+		model.addAttribute("awards", awardService.getAllAwards());
 		return modelAndView;
 	}
 	
@@ -190,6 +218,8 @@ public class PageController {
 	public ModelAndView getPartnerWithUsPage(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/apply-for-partner-with-us");
 		model.addAttribute("partnerWithUsDTO", new PartnerWithUsDTO());
+		model.addAttribute("awards", awardService.getAllAwards() );
+
 		return modelAndView;
 	}
 	
@@ -197,6 +227,8 @@ public class PageController {
 	public ModelAndView getSchoolFeeFinancingPage(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/apply-for-school-fee-financing");
 		model.addAttribute("schoolFeeFinancingDTO", new SchoolFeeFinancingDTO());
+		model.addAttribute("awards", awardService.getAllAwards() );
+
 		return modelAndView;
 	}
 	
@@ -204,6 +236,8 @@ public class PageController {
 	public ModelAndView getApplyForSkillEnhancementPage(Model model) {
 		ModelAndView modelAndView = new ModelAndView("dynamicPages/apply-for-skill-enhancement");
 		model.addAttribute("skillEnhancementDTO", new SkillEnhancementDTO());
+		model.addAttribute("awards", awardService.getAllAwards() );
+
 		return modelAndView;
 	}
 	
@@ -219,21 +253,36 @@ public class PageController {
 		model.addAttribute("csrLeadsDTO", new CSRLeadsDTO());
 		return modelAndView;
 	}
+	@GetMapping("/e-nach")
+	public ModelAndView getENachPage(Model model) {
+		ModelAndView modelAndView = new ModelAndView("dynamicPages/e-nach");
+		model.addAttribute("eNachDTO", new ENachDTO());
+		return modelAndView;
+	}
+
+	@GetMapping("/restructuring-of-loans-step1")
+	public ModelAndView getRestructuringOfLoansPage(Model model) {
+		ModelAndView modelAndView = new ModelAndView("dynamicPages/restructuring-of-loans");
+		model.addAttribute("restructuringOfLoansDTO", new RestructuringOfLoansDTO());
+		return modelAndView;
+	}
 	
+	@GetMapping("/moratorium-facility-2020")
+	public ModelAndView getMoratoriumFacilityPage(Model model) {
+		ModelAndView modelAndView = new ModelAndView("dynamicPages/moratorium-facility-2020");
+		model.addAttribute("moratoriumFacilityDTO", new MoratoriumFacilityDTO());
+		return modelAndView;
+	}
+
+	@GetMapping("/moratorium-facility-deregister")
+	public ModelAndView getMoratoriumFacilityDeregisterPage(Model model) {
+		ModelAndView modelAndView = new ModelAndView("dynamicPages/moratorium-facility-deregister");
+		model.addAttribute("moratoriumDeregisterDTO", new MoratoriumDeregisterDTO());
+		return modelAndView;
+	}
 	
 	/*
-	 * @GetMapping("/viewDynamicPages/{extractedFileName}") public ModelAndView
-	 * getDynamicPage(@PathVariable("extractedFileName") String extractedFileName,
-	 * Model model) { ModelAndView modelAndView = new
-	 * ModelAndView("dynamicPages/"+extractedFileName);
-	 * 
-	 * // model.addAttribute("applyNowGeneralDTO", new ApplyNowGeneralDTO());
-	 * model.addAttribute("executiveEducationLoanDTO", new
-	 * ExecutiveEducationLoanDTO()); //
-	 * model.addAttribute("educationInstitutionLoanDTO", new
-	 * EducationInstitutionLoanDTO()); return modelAndView;
-	 * 
-	 * }
-	 * 
-	 */
+	 * Page for all forms
+	*/
+	
 }
